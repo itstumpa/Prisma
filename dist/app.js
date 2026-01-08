10315dc27b418b0fac1ff63699e0b115c10482dd
itstumpa <tumpadas1fiverr@gmail.com>
2026-01-08 12:30:58 +0600

    update

--- dist/app.js
+++ dist/app.js
@@ -1,4 +1,6 @@
 "use strict";
+import 'dotenv/config';
+
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
